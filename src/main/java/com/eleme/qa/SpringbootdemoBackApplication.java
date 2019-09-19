3f7252657d36abb7b001a4bba03d55d15bdd9f83
刘亮 <liang.liush@ele.me>
2019-09-19 13:08:07 +0800

    init springboot project

---
+++ src/main/java/com/eleme/qa/SpringbootdemoBackApplication.java
@@ -0,0 +1,13 @@
+package com.eleme.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleme/qa/SpringbootdemoBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eleme.qa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdemoBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
