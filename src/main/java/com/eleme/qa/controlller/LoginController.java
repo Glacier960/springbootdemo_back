b6568440c74268602f47905d2869077965b4d417
刘亮 <liang.liush@ele.me>
2019-09-19 13:53:41 +0800

    user business

---
+++ src/main/java/com/eleme/qa/controlller/LoginController.java
@@ -0,0 +1,27 @@
+package com.eleme.qa.controlller;
+
+import com.eleme.qa.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
+
+import com.eleme.qa.pojo.User;
+import java.util.Objects;
+
+@Controller
+public class LoginController {
+    @CrossOrigin
+    @PostMapping(value = "api/login")
+    @ResponseBody
+    public Result login(@RequestBody User requestUser){
+        String username = requestUser.getUsername();
+        username = HtmlUtils.htmlEscape(username);
+        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
+    }
+}
